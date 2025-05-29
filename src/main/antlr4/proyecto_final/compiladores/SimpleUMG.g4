50d80c526b968926ac22b74db00e0681f2ed7a78
COMPILADORES <COMPILADORES@DESKTOP-1VC72CJ.localdomain>
2025-05-28 21:10:52 -0600

    segundo prototipo

--- src/main/antlr4/proyecto_final/compiladores/SimpleUMG.g4
+++ src/main/antlr4/proyecto_final/compiladores/SimpleUMG.g4
@@ -4,8 +4,50 @@ grammar SimpleUMG;
 package proyecto_final.compiladores;
 }
 
-programa: bloque;
-bloque: '{' '}';
+// Regla inicial explícita (para evitar problemas con StartContext)
+start: programa EOF;
 
-IDENT: [a-zA-Z]+;
-WS: [ \t\r\n]+ -> skip;
+// Reglas del parser
+programa: librerias? PROGRAMA tipo IDENT bloque;
+
+librerias: libreria+;
+libreria: LIB tipo IDENT '(' parametros? ')' ';';
+parametros: parametro (',' parametro)*;
+parametro: tipo IDENT;
+
+bloque: '{' (declaracion | sentencia)* '}';
+
+declaracion: tipo IDENT ';';
+
+sentencia: asignacion
+         | si
+         | mientras
+         | retorno
+         | llamada ';';
+
+asignacion: IDENT '=' expresion ';';
+
+si: IF '(' expresion ')' bloque (ELSE bloque)?;
+
+mientras: WHILE '(' expresion ')' bloque;
+
+retorno: RETURN expresion ';';
+
+llamada: IDENT '(' (expresion (',' expresion)*)? ')';
+
+expresion: '(' expresion ')'                                  #parenExpr
+         | op=('-'|'!') expresion                             #unariaExpr
+         | izquierda=expresion op=('*'|'/') derecha=expresion #multiplicativaExpr
+         | izquierda=expresion op=('+'|'-') derecha=expresion #aditivaExpr
+         | izquierda=expresion op=('<'|'<='|'>'|'>=') derecha=expresion #relacionalExpr
+         | izquierda=expresion op=('=='|'!=') derecha=expresion #igualdadExpr
+         | izquierda=expresion '&&' derecha=expresion         #andExpr
+         | izquierda=expresion '||' derecha=expresion         #orExpr
+         | llamada                                            #llamadaExpr
+         | IDENT                                              #identExpr
+         | literal                                            #literalExpr
+         ;
+
+literal: NUMERO | FLOAT_NUM | CADENA | BOOLEANO;
+
+tipo: INT | FLOAT | BOOL | STRING;
--- target/generated-sources/antlr4/proyecto_final/compiladores/SimpleUMGBaseListener.java
+++ target/generated-sources/antlr4/proyecto_final/compiladores/SimpleUMGBaseListener.java
@@ -19,6 +19,18 @@ public class SimpleUMGBaseListener implements SimpleUMGListener {
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
+	@Override public void enterStart(SimpleUMGParser.StartContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitStart(SimpleUMGParser.StartContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
 	@Override public void enterPrograma(SimpleUMGParser.ProgramaContext ctx) { }
 	/**
 	 * {@inheritDoc}
@@ -31,6 +43,54 @@ public class SimpleUMGBaseListener implements SimpleUMGListener {
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
+	@Override public void enterLibrerias(SimpleUMGParser.LibreriasContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitLibrerias(SimpleUMGParser.LibreriasContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterLibreria(SimpleUMGParser.LibreriaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitLibreria(SimpleUMGParser.LibreriaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterParametros(SimpleUMGParser.ParametrosContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitParametros(SimpleUMGParser.ParametrosContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterParametro(SimpleUMGParser.ParametroContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitParametro(SimpleUMGParser.ParametroContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
 	@Override public void enterBloque(SimpleUMGParser.BloqueContext ctx) { }
 	/**
 	 * {@inheritDoc}
@@ -38,6 +98,246 @@ public class SimpleUMGBaseListener implements SimpleUMGListener {
 	 * <p>The default implementation does nothing.</p>
 	 */
 	@Override public void exitBloque(SimpleUMGParser.BloqueContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterDeclaracion(SimpleUMGParser.DeclaracionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitDeclaracion(SimpleUMGParser.DeclaracionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterSentencia(SimpleUMGParser.SentenciaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitSentencia(SimpleUMGParser.SentenciaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterAsignacion(SimpleUMGParser.AsignacionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitAsignacion(SimpleUMGParser.AsignacionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterSi(SimpleUMGParser.SiContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitSi(SimpleUMGParser.SiContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterMientras(SimpleUMGParser.MientrasContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitMientras(SimpleUMGParser.MientrasContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterRetorno(SimpleUMGParser.RetornoContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitRetorno(SimpleUMGParser.RetornoContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterLlamada(SimpleUMGParser.LlamadaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitLlamada(SimpleUMGParser.LlamadaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterIdentExpr(SimpleUMGParser.IdentExprContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitIdentExpr(SimpleUMGParser.IdentExprContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterUnariaExpr(SimpleUMGParser.UnariaExprContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitUnariaExpr(SimpleUMGParser.UnariaExprContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterLiteralExpr(SimpleUMGParser.LiteralExprContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitLiteralExpr(SimpleUMGParser.LiteralExprContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterLlamadaExpr(SimpleUMGParser.LlamadaExprContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitLlamadaExpr(SimpleUMGParser.LlamadaExprContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterMultiplicativaExpr(SimpleUMGParser.MultiplicativaExprContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitMultiplicativaExpr(SimpleUMGParser.MultiplicativaExprContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterOrExpr(SimpleUMGParser.OrExprContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitOrExpr(SimpleUMGParser.OrExprContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterIgualdadExpr(SimpleUMGParser.IgualdadExprContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitIgualdadExpr(SimpleUMGParser.IgualdadExprContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterRelacionalExpr(SimpleUMGParser.RelacionalExprContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitRelacionalExpr(SimpleUMGParser.RelacionalExprContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterParenExpr(SimpleUMGParser.ParenExprContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitParenExpr(SimpleUMGParser.ParenExprContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterAditivaExpr(SimpleUMGParser.AditivaExprContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitAditivaExpr(SimpleUMGParser.AditivaExprContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterAndExpr(SimpleUMGParser.AndExprContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitAndExpr(SimpleUMGParser.AndExprContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterLiteral(SimpleUMGParser.LiteralContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitLiteral(SimpleUMGParser.LiteralContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterTipo(SimpleUMGParser.TipoContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitTipo(SimpleUMGParser.TipoContext ctx) { }
 
 	/**
 	 * {@inheritDoc}
